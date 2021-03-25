cd16a6180114dcd2aee8ba6883c7f24fc6779c77
bucktoothsir <bucktoothsir@gmail.com>
2021-03-24 23:13:22 -0700

    2021.3.24

--- 1052_Grumpy_Bookstore_Owner.py
+++ 1052_Grumpy_Bookstore_Owner.py
@@ -2,35 +2,27 @@ class Solution:
     def maxSatisfied(self, customers: List[int], grumpy: List[int], X: int) -> int:
         if len(customers) <= X:
             return sum(customers)
-        res = 0
-        for i, c in enumerate(customers):
-            if grumpy[i] == 0:
-                res += customers[i]
-                
-        temp = res
+        satisfied = 0
+        not_satisfied = 0
         for i in range(X):
             if grumpy[i] == 1:
-                temp += customers[i]
-        if temp > res:
-            res = temp
-        
+                not_satisfied += customers[i]
+            else:
+                satisfied += customers[i]
         i = 0
         j = X - 1
-        
+        temp = not_satisfied
         while j < len(customers):
             if grumpy[i] == 1:
                 temp -= customers[i]
             i += 1
             j += 1
+                
             if j < len(customers):
                 if grumpy[j] == 1:
                     temp += customers[j]
-                    if temp > res:
-                        res = temp
-        return res
-                
-            
-                    
-        
-        
-
+                    if temp > not_satisfied:
+                        not_satisfied = temp
+                else:
+                    satisfied += customers[j]
+        return  satisfied + not_satisfied
